6a75fc70df4162728cedbcc39432fcfb6be6baf9
silverej <35910739+silverej@users.noreply.github.com>
2018-01-29 13:41:54 +0800

    services fix

--- html/services.php
+++ html/services.php
@@ -15,7 +15,7 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="../js/services.js"></script>
-<a name = "SServices" id = "SServices"></a>
+
 <div id = "SServices1">
 
 <div id = "servicesHeader">
