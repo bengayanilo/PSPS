22c40c6f4609f734c5b8a419bff3f180245af515
khmetalchocobo <35910859+khmetalchocobo@users.noreply.github.com>
2018-01-29 17:41:26 +0800

    register(updated) -> update onti

--- html/register(updated).php
+++ html/register(updated).php
@@ -10,7 +10,7 @@
 <link rel="stylesheet" type="text/css" href="../css/register(updated).css">
 <link rel="icon" href="../images/logo.png">
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-<script src="../js/register.js"></script>
+<script src="../js/register(updated).js"></script>
 <!--<script type="text/javascript" src="../js/register.js"></script>-->
 </head>
 <?php include("../html/topbar.php"); ?>
@@ -38,28 +38,24 @@
 		<label><b>Repeat Password</b></label>
 		<input type="password" placeholder="Repeat Password" id="psw-repeat" name="psw-repeat" required>
 		
-		<button type="button"  id="topage2">Next</button>
-		
-		
-		
 	</div>
 
 	<div id="accountType">
 
 		<!--Select Account Type-->
 		<p>Select Account Type</p>
-		<input type="radio" name="accountType" value="pat"> Patient<br>
-		<input type="radio" name="accountType" value="psy"> Psychologist<br>
-		<input type="radio" name="accountType" value="int"> Intern<br>
-		<input type="radio" name="accountType" value="rev"> Review Applicant<br>
 
-		<BR>
-		<button type="button"  id="backtopage1">Previous</button>				
-		<button type="button"  id="topage3">Next</button>
+			<input type="radio" id = "patientradbtn" name = "accountType" value="pat"> Patient <br>
+			<input type="radio" id = "psyradbtn" name = "accountType" value="psy"> Psychologist <br>
+			<input type="radio"	id = "intradbtn" name = "accountType" value="int"> Intern <br>
+			<input type="radio" id = "revradbtn" name = "accountType" value="rev"> Review Applicant <br>
+
+		<br>	
+		<button type="button"  id="submit">Submit</button>
 		<BR>
 
-	</div>
-	
+	</div>	
+
 	<div id="patient_register_primary">
 
 		<!--Primary Patient Account Information-->
@@ -89,9 +85,8 @@
 
 		<label><b>Contact Number</b></label>
 		<input type="reg-text" placeholder="Enter Contact Number" name="contact" required>
-		
-		<button type="button"  id="topage1">Previous</button>
-		<button type="button"  id="topage3">Next</button>
+	
+		<button type="button"  id="topatient2">Next</button>
 		
 	</div>
 
@@ -124,17 +119,91 @@
 		<label><b>School</b></label>
 		<input type="reg-text" placeholder="Enter School" name="school">
 
-		<label><b>School</b></label>
+		<label><b>Year Graduated</b></label>
 		<input type="reg-text" placeholder="Enter School" name="school">
 
 		<label><b>Other Information</b></label>
 		<input type="reg-text" placeholder="Enter other information" name="other_info">
 		
-		<button type="button"  id="topage1">Previous</button>
-		<button type="button"  id="topage3">Next</button>
+		<button type="button"  id="topatient1">Previous</button>
 		
 	</div>
 
+	<div id="intern_register_primary">
+
+		<!--Primary intern Account Information-->
+		<label><b>Intern Name</b></label>
+		<input type="reg-text" placeholder="Enter First Name" name="firstName" required>
+
+		<label><b>Middle Name</b></label>
+		<input type="reg-text" placeholder="Enter Middle Name" name="middleName" required>
+
+		<label><b>Last Name</b></label>
+		<input type="reg-text" placeholder="Enter Last Name" name="lastName" required>
+
+		<br><br><label><b>Gender</b></label>
+		<select name="gender">
+			<option value="male">Male</option>
+			<option value="female">Female</option>
+		</select><br><br>
+
+		<label><b>Birth Date</b></label>
+		<input type="reg-text" placeholder="Enter your birthdate" name="birthday" required>
+
+		<label><b>Age</b></label>
+		<input type="reg-text" placeholder="Enter Age" name="age" required>
+
+		<label><b>Address</b></label>
+		<input type="reg-text" placeholder="Enter Address" name="address" required>
+
+		<label><b>Contact Number</b></label>
+		<input type="reg-text" placeholder="Enter Contact Number" name="contact" required>
+
+		<button type="button"  id="tointern2">Next</button>
+
+	</div>
+
+	<div id="intern_register_secondary">
+
+		<!--Secondary Intern Account Information-->
+		<label><b>Religion</b></label>
+		<input type="reg-text" placeholder="Enter Religion" name="religion" required>
+
+		<label><b>Citizenship</b></label>
+		<input type="reg-text" placeholder="Enter Citizenship" name="citizenship" required>
+
+		<br><br><label><b>Marital Status</b></label>
+		<select name="marital_status">
+			<option value="male">Single</option>
+			<option value="female">Married</option>
+			<option value="female">Divorced</option>
+			<option value="female">Widowed</option>
+		</select><br><br>
+
+		<label><b>Occupation</b></label>
+		<input type="reg-text" placeholder="Enter Occupation" name="occupation">
+
+		<label><b>Company Name</b></label>
+		<input type="reg-text" placeholder="Enter Company" name="company_name">
+
+		<label><b>School (Graduate School)</b></label>
+		<input type="reg-text" placeholder="Enter Graduate School" name="graduate_school">
+
+		<label><b>Year Graduated</b></label>
+		<input type="reg-text" placeholder="Enter year graduated in Graduate School" name="year_graduate">
+
+		<label><b>School (College)</b></label>
+		<input type="reg-text" placeholder="Enter College" name="college_school">
+
+		<label><b>Year Graduated</b></label>
+		<input type="reg-text" placeholder="Enter year graduated from College" name="year_college">
+
+		<label><b>Other Information</b></label>
+		<input type="reg-text" placeholder="Enter other information" name="other_info">
+
+		<button type="button"  id="topage1">Previous</button>
+
+	</div>
 <!--
 	<div id="reg3">
 		<!--Select Account Type
