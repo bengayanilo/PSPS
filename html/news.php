5e671a01f86433327adeaa65f0d42110f887ad40
silverej <35910739+silverej@users.noreply.github.com>
2018-02-05 22:59:12 +0800

    news section redesign

--- html/news.php
+++ html/news.php
@@ -24,24 +24,72 @@
 	</div>
 </div>
 <div id = "absolute-positioning">
-<div class = "newsContainer">
-<div class = "newsArticles">
-<div class = "newsHeadline">
-<p class = "newsTitle">News Title Here</p>
-<p class = "newsDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-<div class = "seeMoreDiv" >
-<a href = "javascript:;" id = "moreArticle1"><span>See full article >></span></a>
-</div>
-</div>
-</div>
-<div class = "newsContent" id = "mainArticle1">
-<p class = "newsMain">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-<a href = "javascript:;" id = "lessArticle1" class = "seeLessBtn"><span><< See less</span></a>
-</div>
+
+<div class = "newsRow">
+
+	<div class = "newsContainer">
+
+		<div class = "newsArticles"> 
+			<img id = "news1" src = "../images/news/news1.jpg">
+			<div class = "newsHeadline">
+				<div class = "newsTitle">
+					<p><span>News Title Here</span></p>		
+				</div>
+				<p class = "newsDesc">The future professional entrepreneurs
+									The making of practicing psychologists
+									The private practitioners
+									The freelancers
+
+									...will be changing the picture of psychology in the Philippines...go for more practitioners...
+
+									from the seminar - ins and outs of private practice of psychology in the philippines</p>
+				<div class = "seeMoreDiv" >
+					<a href = "javascript:;" id = "moreArticle1" class = "moreArticle"><span>Read More</span></a>
+				</div>
+			</div>
+		</div>
+		<div class = "newsContent" id = "mainArticle1">
+			<p class = "newsMain">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<a href = "javascript:;" id = "lessArticle1" class = "seeLessBtn"><span>See less</span></a>
+		</div>
+	</div>
+
 </div>
 
 <BR>
 
+<div class = "newsRow">
+
+	<div class = "newsContainer">
+
+		<div class = "newsArticles"> 
+			<img id = "news1" src = "../images/news/news2.jpg">
+			<div class = "newsHeadline">
+				<div class = "newsTitle">
+					<p><span>News Title Here</span></p>		
+				</div>
+				<p class = "newsDesc">The future professional entrepreneurs
+									The making of practicing psychologists
+									The private practitioners
+									The freelancers
+
+									...will be changing the picture of psychology in the Philippines...go for more practitioners...
+
+									from the seminar - ins and outs of private practice of psychology in the philippines</p>
+				<div class = "seeMoreDiv" >
+					<a href = "javascript:;" id = "moreArticle2" class = "moreArticle"><span>Read More</span></a>
+				</div>
+			</div>
+		</div>
+		<div class = "newsContent" id = "mainArticle2">
+			<p class = "newsMain">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<a href = "javascript:;" id = "lessArticle2" class = "seeLessBtn"><span>See less</span></a>
+		</div>
+	</div>
+
+</div>
+
+<!--<div class = "newsRow">
 <div class = "newsContainer">
 <div class = "newsArticles">
 <div class = "newsHeadline">
@@ -57,58 +105,105 @@
 <a href = "javascript:;" id = "lessArticle2" class = "seeLessBtn"><span><< See less</span></a>
 </div>
 </div>
+</div>-->
 
 <BR>
 
-<div class = "newsContainer">
-<div class = "newsArticles">
-<div class = "newsHeadline">
-<p class = "newsTitle">News Title Here</p>
-<p class = "newsDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-<div class = "seeMoreDiv">
-<a href = "javascript:;" id = "moreArticle3"><span>See full article >></span></a>
-</div>
-</div>
-</div>
-<div class = "newsContent" id = "mainArticle3">
-<p class = "newsMain">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-<a href = "javascript:;" id = "lessArticle3" class = "seeLessBtn"><span><< See less</span></a>
-</div>
+<div class = "newsRow">
+
+	<div class = "newsContainer">
+
+		<div class = "newsArticles"> 
+			<img id = "news3" src = "../images/news/news3.jpg">
+			<div class = "newsHeadline">
+				<div class = "newsTitle">
+					<p><span>News Title Here</span></p>		
+				</div>
+				<p class = "newsDesc">The future professional entrepreneurs
+									The making of practicing psychologists
+									The private practitioners
+									The freelancers
+
+									...will be changing the picture of psychology in the Philippines...go for more practitioners...
+
+									from the seminar - ins and outs of private practice of psychology in the philippines</p>
+				<div class = "seeMoreDiv" >
+					<a href = "javascript:;" id = "moreArticle3" class = "moreArticle"><span>Read More</span></a>
+				</div>
+			</div>
+		</div>
+		<div class = "newsContent" id = "mainArticle3">
+			<p class = "newsMain">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<a href = "javascript:;" id = "lessArticle3" class = "seeLessBtn"><span>See less</span></a>
+		</div>
+	</div>
+
 </div>
 
 <BR>
 
-<div class = "newsContainer">
-<div class = "newsArticles">
-<div class = "newsHeadline">
-<p class = "newsTitle">News Title Here</p>
-<p class = "newsDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-<div class = "seeMoreDiv">
-<a href = "javascript:;" id = "moreArticle4"><span>See full article >></span></a>
-</div>
-</div>
-</div>
-<div class = "newsContent" id = "mainArticle4">
-<p class = "newsMain">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-<a href = "javascript:;" id = "lessArticle4" class = "seeLessBtn"><span><< See less</span></a>
-</div>
+<div class = "newsRow">
+
+	<div class = "newsContainer">
+
+		<div class = "newsArticles"> 
+			<img id = "news4" src = "../images/news/news4.jpg">
+			<div class = "newsHeadline">
+				<div class = "newsTitle">
+					<p><span>News Title Here</span></p>		
+				</div>
+				<p class = "newsDesc">The future professional entrepreneurs
+									The making of practicing psychologists
+									The private practitioners
+									The freelancers
+
+									...will be changing the picture of psychology in the Philippines...go for more practitioners...
+
+									from the seminar - ins and outs of private practice of psychology in the philippines</p>
+				<div class = "seeMoreDiv" >
+					<a href = "javascript:;" id = "moreArticle4" class = "moreArticle"><span>Read More</span></a>
+				</div>
+			</div>
+		</div>
+		<div class = "newsContent" id = "mainArticle4">
+			<p class = "newsMain">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<a href = "javascript:;" id = "lessArticle4" class = "seeLessBtn"><span>See less</span></a>
+		</div>
+	</div>
+
 </div>
 
 <BR>
 
-<div class = "newsArticles">
-<div class = "newsHeadline">
-<p class = "newsTitle">News Title Here</p>
-<p class = "newsDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-<div class = "seeMoreDiv">
-<a href = "javascript:;" id = "moreArticle5"><span>See full article >></span></a>
-</div>
-</div>
-</div>
-<div class = "newsContent" id = "mainArticle5">
-<p class = "newsMain">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-<a href = "javascript:;" id = "lessArticle5" class = "seeLessBtn"><span><< See less</span></a>
-</div>
+<div class = "newsRow">
+
+	<div class = "newsContainer">
+
+		<div class = "newsArticles"> 
+			<img id = "news5" src = "../images/news/news5.jpg">
+			<div class = "newsHeadline">
+				<div class = "newsTitle">
+					<p><span>News Title Here</span></p>		
+				</div>
+				<p class = "newsDesc">The future professional entrepreneurs
+									The making of practicing psychologists
+									The private practitioners
+									The freelancers
+
+									...will be changing the picture of psychology in the Philippines...go for more practitioners...
+
+									from the seminar - ins and outs of private practice of psychology in the philippines</p>
+				<div class = "seeMoreDiv" >
+					<a href = "javascript:;" id = "moreArticle5" class = "moreArticle"><span>Read More</span></a>
+				</div>
+			</div>
+		</div>
+		<div class = "newsContent" id = "mainArticle5">
+			<p class = "newsMain">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<a href = "javascript:;" id = "lessArticle5" class = "seeLessBtn"><span>See less</span></a>
+		</div>
+	</div>
+
 </div>
 
 
