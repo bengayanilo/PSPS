a64b8b59c6d8ae0b0a795ef94b9108c9b947a166
Khaosneel <35053804+Khaosneel@users.noreply.github.com>
2018-02-05 15:35:28 +0800

    .

--- chatjax/index.php
+++ chatjax/index.php
@@ -2,7 +2,7 @@
     session_start();
     if(!isset($_SESSION['username']))
     {
-        header('location: ../html/home.php');
+        header('location: ../html/register.php');
     }
     elseif(!isset($_GET['target']) || !isset($_GET['chatid']))
     {
