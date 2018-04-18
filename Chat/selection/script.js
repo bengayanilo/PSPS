e9c2d3d65178d2fd7ec6c73d78feb5e8f1cc32a4
Khaosneel <35053804+Khaosneel@users.noreply.github.com>
2018-04-18 10:46:19 +0800

    Addition of chat list

---
+++ Chat/selection/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	setInterval(function(){
+		$.ajax({
+			type: "POST",
+			url: 'list.php',
+			dataType: "JSON",
+			success: function(data)
+			{
+				var output = '';
+				for(var i in data){
+					output +='<a href="javascript:;" class="select_chat">'+
+							        '<div class="media">'+
+							            '<div class="media-left">'+
+							                '<p class="image is-96x96">'+
+							                    '<img src="https://bulma.io/images/placeholders/128x128.png">'+
+							            	'</p>'+
+							            '</div>'+
+							            '<div class="media-content">'+
+							                '<div class="content">'+
+							                    '<p>'+
+							                        '<strong>Person Name</strong> <small>@personname</small> <!--<small class="time_passed">31m</small>-->'+
+							                        '<br>'+
+							                        '<span class="chat_preview truncate"><span class="name_of_sender">Person Name: </span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</span>'+
+							                    '</p>'+
+							                '</div>'+
+							            '</div>'+
+							            '<div class="media-right">'+
+							                '<small class="time_received">10:00PM</small>'+
+							            '</div>'+
+							        '</div>'+
+							    '</a>';	
+				}
+				$('#log').html(output);
+			}
+		});
+	}, 100);
+});
