590661506ae4ba285e20d68851380453f399e456
Khaosneel <35053804+Khaosneel@users.noreply.github.com>
2018-06-05 15:55:20 +0800

    error trapped sending of messages to chat

--- Authentication/Register/register.php
+++ Authentication/Register/register.php
@@ -10,8 +10,9 @@ $password = md5($password);
 $query = "INSERT INTO tbl_users (user_name,user_email,user_password,user_type,joining_date)
 			VALUES ('$username','$email','$password','$type',NOW())";
 if(mysqli_query($db, $query)){
-	echo "Registered";
-	// $_SESSION['id']=
+	$_SESSION['id']=mysqli_insert_id($db);
+	$_SESSION['type']=$type;
+	echo $_SESSION['id'];
 }
 else{
 	echo "Registration Failed";
--- Chat/room/script.js
+++ Chat/room/script.js
@@ -1,40 +1,48 @@
-$(document).ready(function(){
-	//Gets the GET variable passed 
-	var $_GET = {};
-	if(document.location.toString().indexOf('?') !== -1) {
-		var query = document.location
-						.toString()
-						// get the query string
-						.replace(/^.*?\?/, '')
-						// and remove any existing hash string (thanks, @vrijdenker)
-						.replace(/#.*$/, '')
-						.split('&');
+var $_GET = {};
+if(document.location.toString().indexOf('?') !== -1) {
+	var query = document.location
+					.toString()
+					// get the query string
+					.replace(/^.*?\?/, '')
+					// and remove any existing hash string (thanks, @vrijdenker)
+					.replace(/#.*$/, '')
+					.split('&');
 
-		for(var i=0, l=query.length; i<l; i++) {
-		   var aux = decodeURIComponent(query[i]).split('=');
-		   $_GET[aux[0]] = aux[1];
-		}
+	for(var i=0, l=query.length; i<l; i++) {
+	   var aux = decodeURIComponent(query[i]).split('=');
+	   $_GET[aux[0]] = aux[1];
 	}
+}
+
+$(document).ready(function(){
+	log();
 
 	$('#input').submit(function(e){
 		e.preventDefault();
-		$.ajax({
-			type: "POST",
-			url: 'chat.php',
-			data: $(this).serialize()+"&chat_id="+$_GET['chat_id'],
-			success: function(data)
-			{
-				if(data != "query success"){
-					console.log("insert failed")
+		if($('#message').val().length!=0){
+			$.ajax({
+				type: "POST",
+				url: 'chat.php',
+				data: $(this).serialize()+"&chat_id="+$_GET['chat_id'],
+				success: function(data)
+				{
+					if(data != "query success"){
+						console.log("insert failed")
+					}
+					else{
+						$('#message').val('');
+						log()
+					}
 				}
-				else{
-					$('#message').val('');
-				}
-			}
-		});
+			});
+		}
+		
 	});
 
-	setInterval(function(){
+	setInterval(log, 1000);
+});
+
+function log(){
 		$.ajax({
 			type: "POST",
 			url: 'log.php',
@@ -49,5 +57,4 @@ $(document).ready(function(){
 				$('#log').html(output);
 			}
 		});
-	}, 500);
-});
+	}
