f7384972f3063ae82ae763e51389bc221358272f
Khaosneel <35053804+Khaosneel@users.noreply.github.com>
2018-06-07 14:45:39 +0800

    .

--- Chat/video/index.php
+++ Chat/video/index.php
@@ -4,6 +4,7 @@
 		header('Location: ../selection');
 	}
 ?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
@@ -18,12 +19,12 @@
 	
 	<div id="vid-box"></div>
 
-	<form name="loginForm" id="login" action="#" onsubmit="return login(this);">
+	<form name="loginForm" id="login" onsubmit="return login(this);">
 		<input type="text" name="username" id="username" placeholder="Pick a username!" />
 		<input type="submit" name="login_submit" value="Log In">
 	</form>
 
-	<form name="callForm" id="call" action="#" onsubmit="return makeCall(this);">
+	<form name="callForm" id="call" onsubmit="return makeCall(this);">
 		<input type="text" name="number" placeholder="Enter user to dial!" />
 		<input type="submit" value="Call"/>
 	</form>
