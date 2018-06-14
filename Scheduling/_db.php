cd8e6c4550a34e086898c283a7fa7cd14b42b1a6
Khaosneel <35053804+Khaosneel@users.noreply.github.com>
2018-06-14 11:01:33 +0800

    Prototype scheduler
    Added a test scheduler w/o integration to the overall system

---
+++ Scheduling/_db.php
@@ -0,0 +1,49 @@
+<?php
+$host = "127.0.0.1";
+$port = 3306;
+$username = "root";
+$password = "";
+$database = "psps";
+
+$db = new PDO("mysql:host=$host;port=$port",
+               $username,
+               $password);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// other init
+date_default_timezone_set("UTC");
+session_start();
+
+$db->exec("CREATE DATABASE IF NOT EXISTS `$database`");
+$db->exec("use `$database`");
+
+function tableExists($dbh, $id)
+{
+    $results = $dbh->query("SHOW TABLES LIKE '$id'");
+    if(!$results) {
+        return false;
+    }
+    if($results->rowCount() > 0) {
+        return true;
+    }
+    return false;
+}
+
+$exists = tableExists($db, "appointment");
+
+if (!$exists) {
+
+
+    $db->exec("CREATE TABLE appointment (
+    appointment_id              INTEGER       PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    appointment_start           DATETIME      NOT NULL,
+    appointment_end             DATETIME      NOT NULL,
+    appointment_patient_name    VARCHAR (100),
+    appointment_status          VARCHAR (100) DEFAULT 'free' NOT NULL,
+    appointment_patient_session VARCHAR (100),
+    doctor_id                   INTEGER       NOT NULL
+    );");
+
+
+}
+?>
