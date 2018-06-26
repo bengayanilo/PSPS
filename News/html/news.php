7c2198d2d5b07444b22ce0cd77ab6bb6b6f4c736
ejtubeeg <earljohntubig@gmail.com>
2018-06-26 14:24:56 +0800

    updated news design

--- News/html/news.php
+++ News/html/news.php
@@ -14,22 +14,103 @@
 <body class="news-body">
   <div class="tile news-wrapper is-12" id="news-section">
     <div class="columns columns-wrapper">
+
+      <!-- News Left Side -->
       <div class="column news-left-side">
-        <div class="tile is-parent news-parent-tile">
-          <div class="tile news-card">
-            <div class="news-image-container">
-              <img class="news-image" src="../../News/images/news1.jpg">
-            </div>
-            <div class="news-info">
-              <h1 class="news-title">News Title Here</h1>
-              <p class="news-desc">Short description of news</p>
+        <div class="tile is-vertical news-selector-container">
+          <!-- Featured News -->
+          <div class="tile is-parent news-parent-tile">
+            <div class="tile news-featured-card">
+              <div class="news-featured-image-container">
+                <img class="news-featured-image" src="../../News/images/news1.jpg">
+              </div>
+              <div class="news-featured-info">
+                <h1 class="news-featured-title">Selected News Title Here Selected News Title Here</h1>
+                <p class="news-featured-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
             </div>
           </div>
+          <!-- Featured News End -->
+
+          <!-- Older News Carousel -->
+          <div class="tile is-parent news-carousel-tile">
+            <!-- Older News 1 -->
+            <a class="news-select" href="javascript:;"><div class="tile news-older-card is-vertical">
+              <div class="news-older-image-container">
+                <img class="news-older-image" src="../../News/images/news1.jpg">
+              </div>
+              <div class="news-older-info">
+                <h1 class="news-older-title">News Title Here News Title Here News Title Here</h1>
+              </div>
+            </div></a>
+            <!-- Older News 1 End -->
+
+            <!-- Older News 2 -->
+            <a class="news-select" href="javascript:;"><div class="tile news-older-card is-vertical">
+              <div class="news-older-image-container">
+                <img class="news-older-image" src="../../News/images/news2.jpg">
+              </div>
+              <div class="news-older-info">
+                <h1 class="news-older-title">News Title Here News Title Here News Title Here</h1>
+              </div>
+            </div></a>
+            <!-- Older News 2 End -->
+
+            <!-- Older News 3 -->
+            <a class="news-select" href="javascript:;"><div class="tile news-older-card is-vertical">
+              <div class="news-older-image-container">
+                <img class="news-older-image" src="../../News/images/news3.jpg">
+              </div>
+              <div class="news-older-info">
+                <h1 class="news-older-title">News Title Here News Title Here News Title Here</h1>
+              </div>
+            </div></a>
+            <!-- Older News 3 End -->
+
+            <!-- Older News 4 -->
+            <a class="news-select" href="javascript:;"><div class="tile news-older-card is-vertical">
+              <div class="news-older-image-container">
+                <img class="news-older-image" src="../../News/images/news4.jpg">
+              </div>
+              <div class="news-older-info">
+                <h1 class="news-older-title">News Title Here News Title Here News Title Here</h1>
+              </div>
+            </div></a>
+            <!-- Older News 4 End -->
+
+            <!-- Older News 5 -->
+            <a class="news-select" href="javascript:;"><div class="tile news-older-card is-vertical">
+              <div class="news-older-image-container">
+                <img class="news-older-image" src="../../News/images/news5.jpg">
+              </div>
+              <div class="news-older-info">
+                <h1 class="news-older-title">News Title Here News Title Here News Title Here</h1>
+              </div>
+            </div></a>
+            <!-- Older News 5 End -->
+
+          </div>
+          <!-- Older News Carousel End -->
         </div>
       </div>
-      <div class="column news-right-side">
+      <!-- News Left Side End -->
 
+      <!-- News Right Side -->
+      <div class="column news-right-side">
+        <div class="news-article-content">
+          <h1 class="news-content-title">Selected News Title Here</h1>
+          <br>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <br>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <br>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <br>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
       </div>
+      <!-- News Right Side End -->
+
     </div>
   </div>
 </body>
