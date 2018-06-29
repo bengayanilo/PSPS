7892a8295e0e31bf549253bc7dbb2c99cc1d1ccd
Khaosneel <35053804+Khaosneel@users.noreply.github.com>
2018-06-29 17:38:31 +0800

    .

--- Backend/html/index.php
+++ Backend/html/index.php
@@ -5,7 +5,22 @@
 		<div class="column">
 			<div class="card box">
 				<div class="card-content">
-							<b>HELLO WORLD</b>
+					<b>Upload review materials</b>
+					<form id="form" method=POST enctype=multipart/form-data action="">
+					<div class="level">
+						<div class="file">
+							<label class="file-label">
+								<input class="file-input" type="file" name="fileToUpload" id="fileToUpload">
+								<span class="file-cta">
+									<span class="file-label">
+										Choose a file…
+									</span>
+								</span>
+							</label>
+						</div>
+						<input type="submit" class="button is-primary" name="submit" value="upload">
+					</div>
+					</form>
 				</div>
 			</div>
 		</div>
@@ -19,3 +34,48 @@
 
 </body>
 </html>
+
+<?php
+	if(isset($_POST['submit'])){
+		$target_dir = "../../Static/files/review_materials/";
+		$target_file = $target_dir.basename($_FILES["fileToUpload"]["name"]);
+		$uploadOk = 1;
+		$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+	    if($check !== false) {
+	        echo "File is an image - " . $check["mime"] . ".";
+	        $uploadOk = 1;
+	    } else {
+	        echo "File is not an image.";
+	        $uploadOk = 0;
+	    }
+
+	    // Check if file already exists
+		// if (file_exists($target_file)) {
+		//     echo "Sorry, file already exists.";
+		//     $uploadOk = 0;
+		// }
+		// Check file size
+		if ($_FILES["fileToUpload"]["size"] > 500000) {
+		    echo "Sorry, your file is too large.";
+		    $uploadOk = 0;
+		}
+		// Allow certain file formats
+		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+		&& $imageFileType != "gif" ) {
+		    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+		    $uploadOk = 0;
+		}
+		// Check if $uploadOk is set to 0 by an error
+		if ($uploadOk == 0) {
+		    echo "Sorry, there was an error.";
+		// if everything is ok, try to upload file
+		} else {
+		    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+		    	echo "File uploaded";
+		    } else {
+		        echo "Sorry, there was an error updating your profile.";
+		    }
+		}
+	}
+?>
