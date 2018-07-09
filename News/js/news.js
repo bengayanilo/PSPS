e631ac64dd036183148ea48b661ffd00185f91fd
Khaosneel <35053804+Khaosneel@users.noreply.github.com>
2018-07-09 20:32:43 +0800

    testing fb plugin

--- News/js/news.js
+++ News/js/news.js
@@ -1,3 +1,13 @@
+function loadAPI() {
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+}
+
 $(document).ready(function(){
     $('.news-select').click( function(event){
         var newsid = $(this).attr('id');
@@ -28,5 +38,6 @@ $(document).ready(function(){
                 $('#show-news-here').html(output);
             }
         });
+        loadAPI();
     });
 });
