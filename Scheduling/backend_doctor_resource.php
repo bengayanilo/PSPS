39ad95d194f3a960c619ceca8805f82ff9388ec1
Khaosneel <35053804+Khaosneel@users.noreply.github.com>
2018-07-25 19:10:06 +0800

    doctor manage shifts

---
+++ Scheduling/backend_doctor_resource.php
@@ -0,0 +1,20 @@
+<?php
+require_once '_db.php';
+
+$scheduler_doctors = $db->query('SELECT * FROM tbl_users WHERE user_id='.$_SESSION['id']);
+
+class Resource {}
+
+$result = array();
+
+foreach($scheduler_doctors as $doctor) {
+  $r = new Resource();
+  $r->id = $doctor['user_id'];
+  $r->name = $doctor['user_name'];
+  $result[] = $r;
+}
+
+header('Content-Type: application/json');
+echo json_encode($result);
+
+?>
