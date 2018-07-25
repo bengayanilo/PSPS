5cba2f40a5a350a6b508f92dcc8bd4e67517366a
khmetalchocobo <35910859+khmetalchocobo@users.noreply.github.com>
2018-07-26 00:39:52 +0800

    updated verify.php

--- Authentication/Register/html/verify.php
+++ Authentication/Register/html/verify.php
@@ -1,26 +1,31 @@
 <?php
-	// session_start();
-	// if(isset($_SESSION['id'])){
-	// 	header("Location:../../index.php");
-	// }
   require('../../../Database/config.php');
+
+  if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
+
+    $email = mysql_escape_string($_GET['email']); // Set email variable
+    $hash = mysql_escape_string($_GET['hash']); // Set hash variable
+
+    $search = mysqli_query("SELECT user_email, hash, active FROM tbl_users WHERE user_email='".$email."' AND hash='".$hash."' AND active='0'") or die(mysqli_error());
+    $match  = mysqli_num_rows($search);
+
+      if($match > 0){
+            // We have a match, activate the account
+            mysql_query("UPDATE tbl_users SET active='1' WHERE user_email='".$email."' AND hash='".$hash."' AND active='0'") or die(mysql_error());
+            echo '<script type="text/javascript">
+                    alert("Your account has been activated, you can now login");
+                  </script>';
+        }else{
+            // No match -> invalid url or account has already been activated.
+            echo '<script type="text/javascript">
+                    alert("The url is either invalid or you already have activated your account.");
+                  </script>';
+        }
+
+    }else{
+        // Invalid approach
+        echo '<script type="text/javascript">
+                alert("Invalid approach, please use the link that has been send to your email.");
+              </script>';
+    }
 ?>
-<html>
-<head>
-    <title>NETTUTS > Sign up</title>
-
-</head>
-<body>
-    <!-- start header div -->
-    <div id="header">
-        <h3>NETTUTS > Sign up</h3>
-    </div>
-    <!-- end header div -->
-
-    <!-- start wrap div -->
-    <div id="wrap">
-        
-    </div>
-    <!-- end wrap div -->
-</body>
-</html>
