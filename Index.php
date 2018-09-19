5e7b5180032f678ae59fd0aba8d20abf5c364934
ejtubeeg <earljohntubig@gmail.com>
2018-09-19 13:06:37 +0800

    fix

--- Index.php
+++ Index.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	$_SESSION['url'] = 'http://localhost:8080/Web/';
+	$_SESSION['url'] = 'http://localhost/PSPS/';
 	ini_set('SMTP','myserver');
 	ini_set('smtp_port',25);
 	if($_SESSION['type'] != 'admin'){
