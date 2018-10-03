5372db4bdfd407b21ec2f903f87dee5c18e521e2
Khaosneel <35053804+Khaosneel@users.noreply.github.com>
2018-10-03 17:12:41 +0800

    appointment insertion minor FIX

--- Scheduling/_pickdoctors.php
+++ Scheduling/_pickdoctors.php
@@ -2,13 +2,13 @@
 	
 	require('../../Database/config.php');
 
-	$getdoctors = "SELECT surname, firstname FROM tbl_users WHERE user_type='psy'";
+	$getdoctors = "SELECT user_id,surname, firstname FROM tbl_users WHERE user_type='psy'";
 	$result = $db->query($getdoctors);
 
 	if ($result->num_rows > 0) {
 	// output data of each row
 		while($row = $result->fetch_assoc()) {
-			echo '<option>'. $row['firstname'] . ' ' . $row['surname'] .'</option>';
+			echo '<option value="'.$row['user_id'].'">'. $row['firstname'] . ' ' . $row['surname'] .'</option>';
 		}
 	} 
 	else {
